bf5f67ce6e3a501fca2e41c636ea76392cb3ec3a
byeongdoohan <quden04@gmail.com>
2025-07-02 10:06:49 +0900

    feat: #15 refresh 토큰 관련 추가

--- highgrade/src/main/java/com/example/highgrade/config/RedissonConfig.java
+++ highgrade/src/main/java/com/example/highgrade/config/RedissonConfig.java
@@ -4,17 +4,23 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private String port;
+
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
         config.setCodec(new StringCodec());
         config.useSingleServer()
-            .setAddress("redis://127.0.0.1:6379");
+            .setAddress("redis://"+host+":"+port);
         return Redisson.create(config);
     }
 }
--- highgrade/src/main/java/com/example/highgrade/service/TokenService.java
+++ highgrade/src/main/java/com/example/highgrade/service/TokenService.java
@@ -2,6 +2,7 @@ package com.example.highgrade.service;
 
 import com.example.highgrade.config.security.TokenProvider;
 import com.example.highgrade.entity.Members;
+import com.example.highgrade.entity.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBucket;
@@ -9,11 +10,13 @@ import org.redisson.api.RedissonClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -22,17 +25,27 @@ public class TokenService {
     private final RedissonClient redissonClient;
     private final TokenProvider tokenProvider;
     private final static long TOKEN_EXPIRE_SECONDS = 60*60;
-
+    private final static long REFRESH_TOKEN_EXPIRE_SECONDS = 60*60*24;
+    private final static String REFRESH_TOKEN_PREFIX = "refreshToken:";
+    private final static String ACCESS_TOKEN_PREFIX = "accessToken:";
 
     @Transactional
     public String login(final Members foundMember) {
+        String email = foundMember.getEmail();
+        String role = foundMember.getRole().name();
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-            foundMember.getEmail(), null, List.of()
+            email, null,
+            List.of(new SimpleGrantedAuthority(role))
         );
-        String token = tokenProvider.createToken(authentication);
-        RBucket<String> bucket = redissonClient.getBucket(token);
-        bucket.set(authentication.getName(), Duration.ofSeconds(TOKEN_EXPIRE_SECONDS));
-        return token;
+
+        String accessToken = tokenProvider.createToken(authentication);
+        String refreshToken = UUID.randomUUID().toString();
+
+        RBucket<String> bucket = redissonClient.getBucket(ACCESS_TOKEN_PREFIX+foundMember.getEmail());
+        bucket.set(accessToken, Duration.ofSeconds(TOKEN_EXPIRE_SECONDS));
+
+        saveRefreshToken(email, refreshToken);
+        return accessToken;
     }
 
     @Transactional
@@ -42,8 +55,27 @@ public class TokenService {
             return ResponseEntity.badRequest().build();
         }
         String token = authHeader.substring(7);
-        RBucket<String> bucket = redissonClient.getBucket(token);
-        bucket.delete();
+        String email = tokenProvider.getAuthentication(token).getName();
+        redissonClient.getBucket(ACCESS_TOKEN_PREFIX+email).delete();
+        deleteRefreshToken(email);
         return ResponseEntity.ok().build();
+    }
+
+    @Transactional
+    public void saveRefreshToken(String email, String refreshToken){
+        RBucket<String> bucket = redissonClient.getBucket(REFRESH_TOKEN_PREFIX+email);
+        bucket.set(refreshToken, Duration.ofSeconds(REFRESH_TOKEN_EXPIRE_SECONDS));
+    }
+
+    @Transactional
+    public String getRefreshToken(String email) {
+        RBucket<String> bucket = redissonClient.getBucket(REFRESH_TOKEN_PREFIX + email);
+        return bucket.get();
+    }
+
+    @Transactional
+    public void deleteRefreshToken(String email) {
+        RBucket<String> bucket = redissonClient.getBucket(REFRESH_TOKEN_PREFIX + email);
+        bucket.delete();
     }
 }
